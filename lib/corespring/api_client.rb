816ffb0c1cbd41c66bdcb6e74bc45114cc2a6013
benburton <benjamin.joseph.burton@gmail.com>
2015-04-21 11:24:10 -0400

    Add support for encrypting options

--- lib/corespring/api_client.rb
+++ lib/corespring/api_client.rb
@@ -1,17 +1,46 @@
 module CoreSpring
   class APIClient
 
-    def self.get_token(client_id, client_secret)
-      result = CoreSpring.post('/auth/access_token', body: {
-        client_id: client_id,
-        client_secret: client_secret  
-      })
-      if result.code == 200
-        JSON.parse(result.body)['access_token']
-      else
-        throw :invalid_credentials
-      end
+    def initialize(client_id, client_secret)
+      @client_id = client_id
+      @client_secret = client_secret
     end
-    
+
+    def get_token
+      if @token.nil?
+        result = CoreSpring.post('/auth/access_token', body: {
+          client_id: @client_id,
+          client_secret: @client_secret
+        })
+        if result.code == 200
+          @token = JSON.parse(result.body)['access_token']
+        else
+          throw :invalid_credentials
+        end
+      end
+      @token
+    end
+
+    def encrypt(options)
+      def digest_key(raw_key)
+        digest = Digest::MD5.new
+        digest.update(raw_key)
+        digest.digest
+      end
+
+      aes = OpenSSL::Cipher::Cipher.new("AES-128-CBC")
+      iv = aes.random_iv
+      iv_hexed = iv.unpack('H*')[0]
+      key = digest_key(@client_secret)
+      aes.encrypt
+      aes.key = key
+      aes.iv = iv
+      cipher = aes.update(JSON.dump(options))
+      cipher << aes.final
+
+      encrypted_hexed = cipher.unpack('H*')[0]
+      "#{encrypted_hexed}--#{iv_hexed}"
+    end
+
   end
 end
--- spec/corespring/api_client_spec.rb
+++ spec/corespring/api_client_spec.rb
@@ -1,34 +1,48 @@
 require 'spec_helper'
 
-describe 'get_token' do
+describe 'CoreSpring::APIClient' do
+
   let(:client_id) { SecureRandom.hex(13) }
   let(:client_secret) { SecureRandom.hex(13) }
+  let(:client) { CoreSpring::APIClient.new(client_id, client_secret) }
 
-  describe 'with valid client_id and client_secret' do
-    let(:token) { SecureRandom.hex(13) }
+  describe 'get_token' do
 
-    before {
-      stub_request(:post, "https://platform.corespring.org/auth/access_token")
-        .with(:body => "client_id=#{client_id}&client_secret=#{client_secret}")
-        .to_return(:status => 200, body: ({ access_token: token }.to_json))
-    }
+    describe 'with valid client_id and client_secret' do
+      let(:token) { SecureRandom.hex(13) }
+      subject { client.get_token }
 
-    it 'returns token' do
-      CoreSpring::APIClient.get_token(client_id, client_secret).should == token
+      before {
+        stub_request(:post, "https://platform.corespring.org/auth/access_token")
+          .with(:body => "client_id=#{client_id}&client_secret=#{client_secret}")
+          .to_return(:status => 200, body: ({ access_token: token }.to_json))
+      }
+
+      it { should == token }
+
+    end
+
+    describe 'with invalid client_id and client_secret' do
+      subject { client.get_token }
+      before {
+        stub_request(:post, "https://staging.corespring.org/auth/access_token")
+          .with(:body => "client_id=#{client_id}&client_secret=#{client_secret}")
+          .to_return(:status => 403, body: ({"code" => 100, "message" => "Invalid credentials", "moreInfo" => ""}.to_json))
+      }
+
+      it 'raises an error' do
+        expect { subject }.to raise_error
+      end
+
     end
 
   end
 
-  describe 'with invalid client_id and client_secret' do
-    before {
-      stub_request(:post, "https://staging.corespring.org/auth/access_token")
-        .with(:body => "client_id=#{client_id}&client_secret=#{client_secret}")
-        .to_return(:status => 403, body: ({"code" => 100, "message" => "Invalid credentials", "moreInfo" => ""}.to_json))
-    }
+  describe 'encrypt' do
+    let(:options) { {} }
+    subject { client.encrypt(options) }
 
-    it 'raises an error' do
-      expect { CoreSpring::APIClient.get_token(client_id, client_secret) }.to raise_error
-    end
+    it { should be_a(String) }
 
   end
 
